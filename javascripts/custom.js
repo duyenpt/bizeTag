02d951deacc42a5a54612399f238edca24960c1b
duyenpt123 <duyenpt2@gmail.com>
2017-04-19 15:40:31 +0700

    add layout

---
+++ javascripts/custom.js
@@ -0,0 +1,14 @@
+/**
+ * Created by duyenpt on 4/19/17.
+ */
+$(document).ready(function(){
+    $('.right-col .bgr-col').mCustomScrollbar();
+
+    $('.navigator-top .slide-nav').slick({
+        dots: false,
+        infinite: true,
+        slidesToScroll:1,
+        variableWidth:true
+    })
+
+});
